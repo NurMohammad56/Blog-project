46741e44cf07e3bf273ef40ab8ab5ac8dc06790b
Nur Mohammad <milonsardar56@gmail.com>
2023-11-28 20:27:51 +0600

    Dynamic navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/byCategory/:categoryID" element={<PostByCategory />} />
-          <Route path="/details/:postID" />
+          <Route path="/details/:postID" element={<DetailsPage />} />
         </Routes>
       </BrowserRouter>
     </div>
